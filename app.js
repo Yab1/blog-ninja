7dd8acae462d94aa275ef8d66155c4bd40357df2
Yab1 <yabilisanu@gmail.com>
2023-12-15 15:21:20 +0300

    Refactor database connection setup for better error handling by using IIFE
    Modify database connection setup to use async/await for improved error
    handling and logging, ensuring better server stability and easier
    debugging. This change enhances the maintainability and robustness of
    the server. No related issues are referenced.

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ require("dotenv").config();
 
 const app = express();
 
-mongoose
-  .connect(process.env.MONGO_DB_URI)
-  .then((result) => {
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("Connected to MongoDB");
     app.listen(3000);
-  })
-  .catch((err) => console.log(err));
+    console.log("Server is running on port 3000");
+  } catch (err) {
+    console.error(err);
+  }
+})();
 
 app.set("view engine", "ejs");
 
